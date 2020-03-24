07a7c37395577090b14ccf5251e59c34f6308868
YanaVaitovich <janaw@mail.ru>
2020-03-24 20:54:11 +0300

    Second commit

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.print("Hello");
         System.out.println("Hello, Galina");
         System.out.println("Hello, Kak dela?");
+        System.out.println("1");
+        System.out.println("2");
+
+
     }
 }
