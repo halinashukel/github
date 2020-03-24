4d7388cd178c045c31fd121002b1324aea4de742
YanaVaitovich <janaw@mail.ru>
2020-03-24 20:45:24 +0300

    Second commit

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello,git");
+        System.out.print("Hello");
         System.out.println("Hello, Galina");
         System.out.println("Hello, Kak dela?");
     }
