161c0b2d716a18f3a85dcad54ba5c00afd3ceabd
YanaVaitovich <janaw@mail.ru>
2020-03-26 20:32:13 +0300

    commit new4

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,11 +2,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello");
         System.out.println("Hello, Galina");
+        System.out.println("Hello, Galina");
+        System.out.println("Hello, Galina");
+        System.out.println("Hello, Galina");
+        System.out.println("Hello, Galina");
+        System.out.println("Hello, Galina");
+        System.out.println("Hello, Galina");
+        System.out.println("Hello, Galina");
+        System.out.println("Hello, Galina");
+        System.out.println("Hello, Galina");
+        System.out.println("Hello, Galina");
+        System.out.println("Hello, Galina");
 
-
-        System.out.println("Hello, thdrhd");
-        System.out.println("Hello, commit new2");        System.out.println("Hello, commit new2");        System.out.println("Hello, commit new2");        System.out.println("Hello, commit new2");        System.out.println("Hello, commit new2");
 
     }
 }
-
